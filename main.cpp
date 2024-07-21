0f7d00cb60565d6b3a79773c168bfe53e2824dd3
motonoub-t <rx_78_bd@yahoo.co.jp>
2024-07-21 11:03:46 +0900

    ファイル操作

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
 Program for Linux
 */
-
+/* ファイル操作プログラムサンプル */
 #include <iostream>
 #include <filesystem>
 #include <fstream>
@@ -21,7 +21,7 @@ std::string GetExecutableFolderPath(void)
     // 実行中のプロセスのシンボリックリンク
     snprintf(link, LINKSIZE, "/proc/%d/exe", getpid() );
 
-    // ファイルパスが得る
+    // ファイルパスを得る
     ssize_t buf_size{readlink( link, path, PATH_MAX)};
 
     
@@ -33,17 +33,52 @@ std::string GetExecutableFolderPath(void)
     else
     {
         std::string path_str_tmp{path};
-        result_path = path_str_tmp;
-        result_path += '\0';
+        size_t lastSeparater = path_str_tmp.find_last_of("\\/");
+        uint32_t pos = path_str_tmp.rfind("\/");
+        result_path = path_str_tmp.erase(pos);
         printf("D [get path] %s\n", result_path.c_str());
     }
 
     return result_path;
 }
 
+void ShowContents(std::string FolderName)
+{
+    printf("showcontents.\n");
+    try
+    {
+        std::filesystem::path fsyspath{FolderName};
+        bool exists{std::filesystem::exists(fsyspath)};
+        bool isdirectory{std::filesystem::is_directory(fsyspath)};
+        printf("exists = %s, isdirectory = %s\n", exists ? "TRUE" : "FALSE", isdirectory ? "TRUE" : "FALSE");
+
+        if (std::filesystem::exists(FolderName) && std::filesystem::is_directory(FolderName))
+        {
+            printf("filesystem\n");
+            for (const auto& entry : std::filesystem::directory_iterator(FolderName))
+            {
+                std::cout << entry.path().filename() << "\n";
+            }
+        }
+        else
+        {
+            std::cerr << "Folder does not exits or is not a directory. " << std::endl;
+        }
+    }
+    catch (const std::filesystem::filesystem_error& fex)
+    {
+        std::cerr << "FileSystem error " << fex.what() << std::endl;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Error Showing file contents: " << ex.what() << std::endl;
+    }
+}
 
 int main(void)
 {
-    GetExecutableFolderPath();
+    std::string folderpath{GetExecutableFolderPath()};
+    printf("folderpath: %s\n", folderpath.c_str());
+    ShowContents(folderpath);
     return 0;
 }
